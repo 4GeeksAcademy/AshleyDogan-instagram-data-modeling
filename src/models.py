ef7b87939230544e572403a0298247595f2455fe
Ashley Dogan <a.a.rappette@gmail.com>
2025-02-27 01:11:52 +0000

    project submit

--- src/models.py
+++ src/models.py
@@ -1,29 +1,46 @@
 import os
 import sys
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, ForeignKey
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+
+class posts(Base):
+    __tablename__ = 'posts'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    captions: Mapped[str] = mapped_column(nullable=True)
+    media: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    timestamp: Mapped[str] = mapped_column(nullable=False)
+    tagged_friends: Mapped[int] = mapped_column(nullable=True)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(nullable=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"))
 
     def to_dict(self):
         return {}
+    
+
+
 
 ## Draw from SQLAlchemy base
 try:
